bbe28d9181d2d562f434737b8e23254c9e5738d2
burakozbudak <burakozbudak25@hacettepe.edu.tr>
2025-07-28 05:33:08 +0300

    main.jsx güncellemesi
    QueryClientProvider ve ReactQueryDevtools ile güncellendi. Mevcut BrowserRouter yapısını korundu

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 
+// QueryClient instance’ı oluştur
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
